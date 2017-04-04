1fe157214bb055b54ae6523c8c799ccdbe1524a6
David Krief <d.krief@live.fr>
2017-04-04 11:09:27 +0200

    ajoutexo

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.get('/', function(req, res){
   res.send('hello World');
 })
 app.get('/users', (req, res) => {
-  res.send('Liste of all Users');
-})
-
+    let people = [{id: 0, firstName: 'Michel'}, {id: 1, firstName: 'Osman'}, {id: 2, firstName: 'Tandi'}, {id: 3, firstName: 'Daniel'}, {id: 4, firstName: 'Faustino'}, {id: 5, firstName: 'Ijacques'}];
+    res.render('people.ejs', {people: people});
+});
 
 app.get('/users/:user', function(req, res) {
     res.render('users.ejs', {user: req.params.user});
