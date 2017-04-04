93eed4905b11df3c78b4191f1146ec5e11ad9de9
David Krief <d.krief@live.fr>
2017-04-04 12:06:46 +0200

    correctiondavid

--- index.js
+++ index.js
@@ -3,12 +3,36 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-  res.send('hello World');
+  res.render('homepage.ejs');
 })
+
+app.get('/about', function(req, res){
+  res.render('about.ejs', {salutation: 'bonjour'});
+})
+
+
 app.get('/users', (req, res) => {
     let people = [{id: 0, firstName: 'Michel'}, {id: 1, firstName: 'Osman'}, {id: 2, firstName: 'Tandi'}, {id: 3, firstName: 'Daniel'}, {id: 4, firstName: 'Faustino'}, {id: 5, firstName: 'Ijacques'}];
     res.render('people.ejs', {people: people});
-});
+  });
+    /*const TabName = user.map(item) =>{
+                    return item.firstName
+  })
+  const TabName2 = TabName.join(',')
+  res.send (TabeName2) */
+
+/*
+app.get('/users/:id', (req,res) =>{
+  const user = users.find(item)=>{
+    return item.id === Number(req.params.id)
+  }}
+ if(user){
+ res.send(user.firstName)
+}
+else{
+res.send('cette page nexiste pas ')
+}
+  */
 
 app.get('/users/:user', function(req, res) {
     res.render('users.ejs', {user: req.params.user});
